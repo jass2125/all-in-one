ab813612c7f802515919bf86c061c9d01f395a44
Anderson Souza <jair_anderson_bs@hotmail.com>
2019-07-27 16:56:18 -0300

    renderizando primeira lista de notícias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 export class AppComponent {
   
+  // @Input()
+  // private tabSelected: string;
   @Output()
   public tab = new EventEmitter<string>();
+  public tabSelected = 'hacker-news';
 
   title = 'all-in-one';
 
   public change(value){
+    this.tabSelected = value
     console.log(value);
     
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { HackerNewsComponent } from './hacker-news/hacker-news.component';
-import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideMenuComponent,
-    HackerNewsComponent,
-    MainComponent
+    HackerNewsComponent
   ],
   imports: [
     BrowserModule,
